b893716e0945f2068285fb817ff052fca2aa5d71
Ariq Mukul <ariqmukul@gmail.com>
2024-01-09 13:29:31 -0500

    added arabic verse of quran to webpage

--- start.py
+++ start.py
@@ -13,7 +13,11 @@ def index():
     
     verse = random.randint(1, 6236)
     quran_translation_data = requests.get(f'http://api.alquran.cloud/v1/ayah/{verse}/en.asad')
-    quran_verse = json.loads(quran_translation_data.content)
+    quran_english_verse = json.loads(quran_translation_data.content)
+    
+    quran_arabic_data = requests.get(f'http://api.alquran.cloud/v1/ayah/{verse}')
+    quran_arabic_verse = json.loads(quran_arabic_data.content)
+    
 
-    print(quran_verse['data']['surah']['number'], quran_verse['data']['numberInSurah'])
-    return render_template('index.html',  data1=hadith, data2=quran_verse)
+    print(quran_english_verse['data']['surah']['number'], quran_english_verse['data']['numberInSurah'])
+    return render_template('index.html',  data1=hadith, data2=quran_english_verse, data3 = quran_arabic_verse)
